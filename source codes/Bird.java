d05fc094f2b0951bef5de8cfc03bf7780a058347
Justin Oue <justinoue621@gmail.com>
2017-03-17 17:53:52 -0400

    Update Bird.java

--- source codes/Bird.java
+++ source codes/Bird.java
@@ -1,11 +1,5 @@
 import greenfoot.*;
 
-/**
- * Write a description of class SkyDude1 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Bird extends Enemy
 {
     SimpleTimer timer = new SimpleTimer();
@@ -18,10 +12,7 @@ public class Bird extends Enemy
         setImage(image);
     }
 
-    /**
-     * Act - do whatever the SkyDude1 wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+
     public void act() 
     {        
         enemyMove();
