290ce6610f416b0f2329a179946d355869dfd469
Justin Oue <justinoue621@gmail.com>
2017-03-17 17:55:58 -0400

    Update CreditTexts.java

--- source codes/CreditTexts.java
+++ source codes/CreditTexts.java
@@ -1,17 +1,8 @@
 import greenfoot.*;
 
-/**
- * Write a description of class CreditTexts here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class CreditTexts extends Texts
 {
-    /**
-     * Act - do whatever the CreditTexts wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+  
     public void act() 
     {
         move(-1);
