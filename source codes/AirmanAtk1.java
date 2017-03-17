74410a2ccfb687f1e2348b9206159cfc7e85ee6c
Justin Oue <justinoue621@gmail.com>
2017-03-17 17:51:09 -0400

    Update AirmanAtk1.java

--- source codes/AirmanAtk1.java
+++ source codes/AirmanAtk1.java
@@ -1,11 +1,6 @@
 import greenfoot.*;
 
-/**
- * Write a description of class airmanAtk1 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class AirmanAtk1 extends EnemyAttacks
 {
     public AirmanAtk1(){
@@ -17,10 +12,7 @@ public class AirmanAtk1 extends EnemyAttacks
         setImage(image);
     }
 
-    /**
-     * Act - do whatever the airmanAtk1 wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+
     public void act() 
     {
         move(-5);
