27edbb7114665df1fe473cbdf1312e282b4498a2
Justin Oue <justinoue621@gmail.com>
2017-03-17 18:00:05 -0400

    Update Rocket.java

--- source codes/Rocket.java
+++ source codes/Rocket.java
@@ -1,11 +1,5 @@
 import greenfoot.*;
 
-/**
- * Write a description of class rocket here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Rocket extends EnemyAttacks
 {
     public Rocket(){
@@ -18,10 +12,6 @@ public class Rocket extends EnemyAttacks
         setImage(image);
     }
 
-    /**
-     * Act - do whatever the rocket wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act() 
     {
         move(-5);
