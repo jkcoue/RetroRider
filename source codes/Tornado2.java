ac699e5b0ab9e78962aaacf5063c1c3f13963b27
Justin Oue <justinoue621@gmail.com>
2017-03-17 18:02:15 -0400

    Update Tornado2.java

--- source codes/Tornado2.java
+++ source codes/Tornado2.java
@@ -1,11 +1,5 @@
 import greenfoot.*;
 
-/**
- * Write a description of class Tornado2 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Tornado2 extends EnemyAttacks
 {
     int direction;
@@ -18,10 +12,7 @@ public class Tornado2 extends EnemyAttacks
         setImage(image);
         direction = 0;
     }
-    /**
-     * Act - do whatever the Tornado2 wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+
     public void act() 
     {
         setRotation(direction);
