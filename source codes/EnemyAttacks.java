a3c49c4fc43c2542681f4f5ec16f96b061e0dba8
Justin Oue <justinoue621@gmail.com>
2017-03-17 17:57:55 -0400

    Update EnemyAttacks.java

--- source codes/EnemyAttacks.java
+++ source codes/EnemyAttacks.java
@@ -1,11 +1,5 @@
 import greenfoot.*;
 
-/**
- * Write a description of class EnemyAttacks here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class EnemyAttacks extends Actor
 {
     /**
