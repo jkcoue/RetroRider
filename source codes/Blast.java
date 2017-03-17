8be7df2d446489ae4a1cd4e55da06e0f26e5fd12
Justin Oue <justinoue621@gmail.com>
2017-03-17 17:54:06 -0400

    Update Blast.java

--- source codes/Blast.java
+++ source codes/Blast.java
@@ -1,11 +1,5 @@
 import greenfoot.*;
 
-/**
- * Write a description of class Blast here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Blast extends Attacks
 {   
     int direction;
@@ -13,10 +7,6 @@ public class Blast extends Attacks
         direction = 0;
     }
 
-    /**
-     * Act - do whatever the Blast wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act() 
     {
         setRotation(direction);
