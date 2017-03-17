e48e6573beb6a42dac435d1064a5eea5c91e9822
Justin Oue <justinoue621@gmail.com>
2017-03-17 17:59:11 -0400

    Update HeroClone.java

--- source codes/HeroClone.java
+++ source codes/HeroClone.java
@@ -1,18 +1,9 @@
 import greenfoot.*;
 
-/**
- * Write a description of class HeroClone here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class HeroClone extends Hero
 {
     SimpleTimer timer = new SimpleTimer();
-    /**
-     * Act - do whatever the HeroClone wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+
     public void act() 
     {
         if (getX() < 400){
