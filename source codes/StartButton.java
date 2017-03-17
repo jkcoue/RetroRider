5506d854fe18b93bfda4832ffe2029528c63b2df
Justin Oue <justinoue621@gmail.com>
2017-03-17 18:00:29 -0400

    Update StartButton.java

--- source codes/StartButton.java
+++ source codes/StartButton.java
@@ -1,11 +1,5 @@
 import greenfoot.*;
 
-/**
- * Write a description of class StartTitle here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class StartButton extends Texts
 {
     int timer = 0;
